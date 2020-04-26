6bf7f47f79f7a216682c741ec1e0191d27cae006
Forgoot <forgoot@mail.ru>
2020-04-26 23:41:42 +0300

    Before methods

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,91 +1,87 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Task {
-    public static void main(String[] args) throws IOException {
+class Task {
+    public static void main(String[] args) throws InterruptedException, IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String userName;
+        System.out.println("Здравствуйте!");
+        Thread.sleep(3000);
+        System.out.println("Введите имя пользователя:");
+        userName = reader.readLine();
 
-        String BadA;
+
+        String userInputA;
+        String userInputB;
+        int changedValueA = 0;
+        int changedValueB = 0;
+        boolean isInputCorrect;
+
+        System.out.println(userName + ", введите первое число от -2147483648 до 2147483647 и нажмите Enter");
 
 
-        System.out.println("Введите первое число от -2147483648 до 2147483647 и нажмите Enter");
-        BadA = reader.readLine();
-
-
-        int A;
-        try {
-            A = Integer.parseInt(BadA);
-        } catch (Exception E) {
-            System.out.println("Ошибка: А не удовлеторяет условиям, попробуйте ещё раз");
-            System.out.println("Введите первое число повторно от -2147483648 до 2147483647 и нажмите Enter");
-            BadA = reader.readLine();
+        do {
             try {
-                A = Integer.parseInt(BadA);
-            } catch (Exception W) {
-                System.out.println("Ошибка: А не удовлеторяет условиям, попробуйте ещё раз");
-                System.out.println("Введите первое число повторно от -2147483648 до 2147483647 и нажмите Enter");
-                BadA = reader.readLine();
-                try {
-                    A = Integer.parseInt(BadA);
-                } catch (Exception Q) {
-                    System.out.println("Программа завершает работу");
-                    System.out.println("Ты слишком тупой, сорри");
-                    return;
-                }
-
+                userInputA = reader.readLine();
+                changedValueA = Integer.parseInt(userInputA);
+                isInputCorrect = true;
+            } catch (Exception E) {
+                System.out.println("Ошибка: число должно принадлежать промежутку от -2147483648 до 2147483647, попробуйте ещё раз");
+                Thread.sleep(2000);
+                System.out.println("Введите первое число повторно и нажмите Enter");
+                isInputCorrect = false;
             }
-
-        }
-
-
-        String BadB;
+        } while (!isInputCorrect);
 
 
-        System.out.println("Введите второе число от -2147483648 до 2147483647 и нажмите Enter");
-        BadB = reader.readLine();
+
+        System.out.println("Введите второе число в промежутке от -2147483648 до 2147483647 и нажмите Enter");
 
 
-        int B;
-        try {
-            B = Integer.parseInt(BadB);
-        } catch (Exception R) {
-            System.out.println("Ошибка: B не удовлеторяет условиям, попробуйте ещё раз");
-            System.out.println("Введите второе число повторно от -2147483648 до 2147483647 и нажмите Enter");
-            BadB = reader.readLine();
+        isInputCorrect = false;
+        while (!isInputCorrect) {
             try {
-                B = Integer.parseInt(BadB);
-            } catch (Exception T) {
-                System.out.println("Ошибка: B не удовлеторяет условиям, попробуйте ещё раз");
-                System.out.println("Введите второе число повторно от -2147483648 до 2147483647 и нажмите Enter");
-                BadB = reader.readLine();
-                try {
-                    B = Integer.parseInt(BadB);
-                } catch (Exception Y) {
-                    System.out.println("программа завершает работу");
-                    System.out.println("Ты слишком тупой, сорри");
-                    return;
-                }
-
-                boolean isZero = B == 0;
-
-                if (isZero) {
-                    System.out.println("Ошибка, второе число не может равняться нулю");
-                    return;
-                }
+                userInputB = reader.readLine();
+                changedValueB = Integer.parseInt(userInputB);
+                isInputCorrect = true;
+            }
+            catch (Exception Y) {
+                System.out.println("Ошибка: число должно принадлежать промежутку от -2147483648 до 2147483647, попробуйте ещё раз");
+                Thread.sleep(2000);
+                System.out.println("Введите второе число повторно и нажмите Enter");
+                isInputCorrect = false;
             }
         }
-        long derivative = (long) A * B;
-        System.out.println("Производное=" + derivative);
-        double quotient = (double) A / B;
+        boolean isZero = changedValueB == 0;
+// объеденить все в один цикл, сделать дополнительную проверку в одном цикле(try+catch/if)
+// меняй isInputCorrect
+// в нужный момент использовать break/continue
+// использовать дебагер(следуй шаг за шагом отследивая значения переменных(состояние программы))
+
+        long derivative = (long) changedValueA * changedValueB;
+        System.out.println("Произведение=" + derivative);
+        double quotient = (double) changedValueA / changedValueB;
         System.out.println("Частное=" + quotient);
-        long sum = (long) A + B;
+        long sum = (long) changedValueA + changedValueB;
         System.out.println("Сумма=" + sum);
-        long residual = A - B;
+        long residual = changedValueA - changedValueB;
         System.out.println("Разноцть=" + residual);
 
 
-
-        System.out.println("Сумма всех чисел выше=" + (derivative + quotient + sum + residual));  }
+        System.out.println("Сумма всех чисел выше=" + (derivative + quotient + sum + residual));
+    }
 }
+//    boolean isZero = changedValueB == 0;
+//                if (isZero) {
+//                        do {
+//                        System.out.println("Ошибка, второе число не может равняться нулю");
+//                        System.out.println("Введите второе число повторно и нажмите Enter");
+//                        userInputB = reader.readLine();
+//                        changedValueB = Integer.parseInt(userInputB);
+//                        isZero = changedValueB == 0;
+//                        }
+//                        while (isZero ==false);
+//                        break;}

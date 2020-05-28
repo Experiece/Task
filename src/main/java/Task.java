f8ca6151cdc44699f6d5494fe24015ca463b148e
Forgoot <forgoot@mail.ru>
2020-05-29 01:13:50 +0300

    More optimization. Replaced 2 methods from task to Operations.java and UserInput.javanput

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,4 @@
 import java.io.IOException;
-import java.util.ArrayList;
-
-import static java.lang.StrictMath.sqrt;
 
 class Task {
     public static void main(String[] args) throws InterruptedException, IOException {
@@ -20,32 +17,10 @@ class Task {
         }
     }
 
-    public static int convertToInt(String inputValue) {
-        int outputValue = 0;
-        try {
-            outputValue = Integer.parseInt(inputValue);
-        } catch (Exception y) {
-            System.out.println("Ошибка: число должно принадлежать промежутку от 0 до 2147483647, попробуйте ещё раз");
-            throw new RuntimeException();
-        }
-        return outputValue;
-    }
 
     public static String createInputRequest(String userName, String inputName) {
         String messageTemplate = "%s, введите %s число от 0 до 2147483647 и нажмите Enter";
         return String.format(messageTemplate, userName, inputName);
-    }
-
-    public static BaseOperationsResult calculateBaseOperations(int valueA, int valueB) {
-        BaseOperationsResult result;
-        result = new BaseOperationsResult();
-
-        result.derivative = (long) valueA * valueB;
-        result.quotient = (double) valueA / valueB;
-        result.sum = (long) valueA + valueB;
-        result.residual = valueA - valueB;
-        result.mod = valueA % valueB;
-        return result;
     }
 }
 // объеденить все в один цикл, сделать дополнительную проверку в одном цикле(try+catch/if)

b7b7d6300840af2f8089cfdda0df086c7a20f969
Forgoot <forgoot@mail.ru>
2020-05-29 00:02:29 +0300

    I removed 1 extra unused method. Removed unnecessary lanes. Ctrl+alt+l'ed whole code.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -13,7 +13,7 @@ class Task {
         while (true) {
             Operations.cases(userName);
             String message = "Хотите ли вы продолжить операции? \n1 - да \n2 - нет";
-            int A = UserInput.getIntFromConsole(message, 1, 2, false);
+            int A = UserInput.getIntFromConsole(message, 1, 2, false, true);
             if (A == 2) {
                 return;
             }
@@ -25,14 +25,14 @@ class Task {
         try {
             outputValue = Integer.parseInt(inputValue);
         } catch (Exception y) {
-            System.out.println("Ошибка: число должно принадлежать промежутку от -2147483648 до 2147483647, попробуйте ещё раз");
+            System.out.println("Ошибка: число должно принадлежать промежутку от 0 до 2147483647, попробуйте ещё раз");
             throw new RuntimeException();
         }
         return outputValue;
     }
 
     public static String createInputRequest(String userName, String inputName) {
-        String messageTemplate = "%s, введите %s от -2147483648 до 2147483647 и нажмите Enter";
+        String messageTemplate = "%s, введите %s число от 0 до 2147483647 и нажмите Enter";
         return String.format(messageTemplate, userName, inputName);
     }
 
@@ -41,49 +41,11 @@ class Task {
         result = new BaseOperationsResult();
 
         result.derivative = (long) valueA * valueB;
-
         result.quotient = (double) valueA / valueB;
-
         result.sum = (long) valueA + valueB;
-
         result.residual = valueA - valueB;
-
-        result.ostatok = valueA % valueB;
-
+        result.mod = valueA % valueB;
         return result;
-    }
-
-    public static int findSimpleMultipliers(int potentialPrimeNumber, int currentValue, int multiplier) {
-        ArrayList<Integer> primeNumbersArr = new ArrayList<>();
-
-        for (potentialPrimeNumber = 2; potentialPrimeNumber < 100; potentialPrimeNumber++) {
-            boolean isPrime = true;
-            for (int i = 2; i <= sqrt(potentialPrimeNumber); i++) {
-                int mod = potentialPrimeNumber % i;
-                if (mod == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-
-            if (isPrime) {
-                System.out.println(potentialPrimeNumber);
-                primeNumbersArr.add(potentialPrimeNumber);
-            }
-        }
-
-        int inputNumber = 97;
-
-        for (int i = 0; i < primeNumbersArr.size(); i++) {
-
-            int mod = inputNumber % primeNumbersArr.get(i);
-            if (mod == 0) {
-                System.out.println(primeNumbersArr.get(i));
-                inputNumber = inputNumber / primeNumbersArr.get(i);
-                i--;
-            }
-        }
-        return inputNumber;
     }
 }
 // объеденить все в один цикл, сделать дополнительную проверку в одном цикле(try+catch/if)
